da87345ba2e1d8973f93ec6ce8125c17e8212a9f
manu <emmanuel.boudreault@polymtl.ca>
2019-11-17 10:37:10 +0100

    Initial commit

---
+++ precache-manifest.bd6ef785c2671074487630aac6df155c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8719937766d02179710d5e6b18c6f3fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0078b2d666c0eaf9229",
+    "url": "/static/css/main.80542d5a.chunk.css"
+  },
+  {
+    "revision": "0b510ddc99a46c10deec",
+    "url": "/static/js/2.fb3f81b6.chunk.js"
+  },
+  {
+    "revision": "d0078b2d666c0eaf9229",
+    "url": "/static/js/main.316f516d.chunk.js"
+  },
+  {
+    "revision": "ce330dc2d103437ec314",
+    "url": "/static/js/runtime-main.f72058a0.js"
+  }
+]);
